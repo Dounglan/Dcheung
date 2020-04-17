401f0249fc08095153939146cb79eb900e8d52ec
Dounglan Cheung <49924135+Dounglan@users.noreply.github.com>
2020-04-17 02:22:27 -0400

    Add files via upload

---
+++ labs/Operator.java
@@ -0,0 +1,36 @@
+// Enumerated type representing possible binary operators. 
+
+public enum Operator {
+  ADD("+"), SUBTRACT("-"), MULTIPLY("*"), DIVIDE("/");
+
+  private String opString;
+
+  Operator(String operator) {
+    this.opString = operator;
+  }
+
+  /**
+   * Return the Operator enum that corresponds to the string representation of
+   * the operator.
+   * 
+   * @param operator Should be one of "+", "-", "*", "/"
+   * @return The corresponding Operator Enum
+   * @throws IllegalArgumentException if the string does not represent a valid
+   *         operator
+   */
+  public static Operator parseOperator(String operator) {
+    for (Operator op : Operator.values()) {
+      if (op.opString.equals(operator)) {
+        return op;
+      }
+    }
+    throw new IllegalArgumentException("Unrecognized operator");
+  }
+
+  /**
+   * Return the string representation of this operator.
+   */
+  public String opString() {
+    return opString;
+  }
+}
